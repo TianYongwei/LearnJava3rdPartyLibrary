a62ed4b3231e7591251325a0a8653f4abce66168
tianyongwei <869358189@qq.com>
2018-03-22 09:41:18 +0800

    upd

--- src/collections/Hello.java
+++
@@ -1,7 +0,0 @@
-package collections;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println(123123123);
-    }
-}
--- src/utils/Hello.java
+++ src/utils/Hello.java
@@ -1,4 +1,9 @@
 package utils;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public class Hello {
+    public static void main(String[] args) {
+//        ArrayUtils.
+    }
 }
